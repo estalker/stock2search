49b3ab3e079188bdfb539117ecffe5f854d504d1
estalker <sergei.knyazev@gmail.com>
2019-10-25 12:04:09 +0300

    Works with moxd rev 2.7

--- _build/data/transport.menu.php
+++ _build/data/transport.menu.php
@@ -5,20 +5,12 @@
  * @package doodles
  * @subpackage build
  */
-$action= $modx->newObject('modAction');
-$action->fromArray(array(
-    'id' => 1,
-    'namespace' => 'doodles',
-    'parent' => 0,
-    'controller' => 'controllers/index',
-    'haslayout' => true,
-    'lang_topics' => 'doodles:default',
-    'assets' => '',
-),'',true,true);
 
 $menu= $modx->newObject('modMenu');
 $menu->fromArray(array(
     'text' => 'doodles',
+    'namespace' => 'doodles',
+    'action' => 'home',
     'parent' => 'components',
     'description' => 'doodles.desc',
     'icon' => 'images/icons/plugin.gif',
@@ -26,7 +18,7 @@ $menu->fromArray(array(
     'params' => '',
     'handler' => '',
 ),'',true,true);
-$menu->addOne($action);
+
 unset($menus);
 
 return $menu;
--- core/components/doodles/controllers/home.class.php
+++ core/components/doodles/controllers/home.class.php
@@ -1,4 +1,37 @@
 <?php
+require_once dirname(dirname(__FILE__)) . '/model/doodles/doodles.class.php';
+/**
+ * @package doodles
+ * @subpackage controllers
+ */
+abstract class DoodlesManagerController extends modExtraManagerController {
+    /** @var Doodles $doodles */
+    public $doodles;
+    public function initialize() {
+        $this->doodles = new Doodles($this->modx);
+
+        $this->addCss($this->doodles->config['cssUrl'].'mgr.css');
+        $this->addJavascript($this->doodles->config['jsUrl'].'mgr/doodles.js');
+        $this->addHtml('<script type="text/javascript">
+        Ext.onReady(function() {
+            Doodles.config = '.$this->modx->toJSON($this->doodles->config).';
+        });
+        </script>');
+        return parent::initialize();
+    }
+    public function getLanguageTopics() {
+        return array('doodles:default');
+    }
+    public function checkPermissions() { return true;}
+}
+/**
+ * @package doodles
+ * @subpackage controllers
+ */
+class IndexManagerController extends DoodlesManagerController {
+    public static function getDefaultController() { return 'home'; }
+}
+
 /**
  * @package doodles
  * @subpackage controllers
